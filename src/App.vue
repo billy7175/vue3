651a101ed2f52317cfef9af7450deb48e6c429bc
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-12-22 02:22:57 +0900

    add tailwind config setting

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <h1 className="flex text-3xl font-bold underline">Hello world!</h1>
+    <h1 class="flex text-3xl font-bold underline">Hello world!</h1>
+    <main-nav />
   </div>
 </template>
+
+<script>
+import MainNav from "@/components/MainNav.vue";
+export default {
+  components: {
+    MainNav,
+  },
+};
+</script>
