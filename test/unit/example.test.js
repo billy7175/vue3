60a9c2ff1acf662842a9bade4e7f16700385772c
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-12-24 00:42:50 +0900

    Add vitest --coverage flag && lint

--- test/unit/example.test.js
+++ test/unit/example.test.js
@@ -2,12 +2,16 @@ import { describe, it, expect } from "vitest";
 import { evenOrOdd, multiply } from "@/example.js";
 describe("basic math", () => {
   it("adds two numbers", () => {
-    expect(evenOrOdd(1 + 1)).toBe('Even');
+    expect(evenOrOdd(1 + 1)).toBe("Even");
+  });
+
+  it("adds two numbers", () => {
+    expect(evenOrOdd(1 + 2)).toBe("Odd");
   });
 });
 
 describe("multiply", () => {
   it("multiplies two numbers together", () => {
-    expect(multiply(2,3)).toBe(6);
+    expect(multiply(2, 3)).toBe(6);
   });
 });
