eee15059251585a9cd5f95fdb18723f0638d7686
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-12-29 01:26:07 +0900

    Test user-event library

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,12 +1,17 @@
 import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
+
 import MainNav from "@/components/MainNav.vue";
 
-describe("MainNav.vue", () => {
+describe("MainNav", () => {
   it("displays company name", () => {
     render(MainNav);
-    // screen.debug();
-    // const name = screen.getByText("Bobo Careers");
-    // expect(name).toBeInTheDocument();
+    const companyName = screen.getByText("Bobo Careers");
+    expect(companyName).toBeInTheDocument();
+  });
+
+  it("displays menu items for navigation", () => {
+    render(MainNav);
     const navigationMenuItems = screen.getAllByRole("listitem");
     const navigationMenuTexts = navigationMenuItems.map(
       (item) => item.textContent
@@ -19,6 +24,26 @@ describe("MainNav.vue", () => {
       "Students",
       "Jobs",
     ]);
-    // expect(screen.getByRole("navigation")).toBeInTheDocument();
+  });
+
+  describe("when the user logs in", () => {
+    it("displays user profile picture", async () => {
+      render(MainNav);
+
+      let profileImage = screen.queryByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).not.toBeInTheDocument();
+
+      const loginButton = screen.getByRole("button", {
+        name: /sign in/i,
+      });
+      await userEvent.click(loginButton);
+
+      profileImage = screen.getByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).toBeInTheDocument();
+    });
   });
 });
