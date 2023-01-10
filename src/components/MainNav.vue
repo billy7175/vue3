84a9cf3273e77e94bb6edb7cb2c724e42c42d618
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2023-01-11 00:59:42 +0900

    Add TheSubnav component && test && font-awesome

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="w-full text-sm">
-    <div class="top-0 left-0 h-16 w-full bg-white">
+    <div class="fixed top-0 left-0 h-16 w-full bg-white">
       <div
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
@@ -24,14 +24,11 @@
 
         <div class="ml-auto flex h-full items-center">
           <profile-image v-if="isLoggedIn" />
-          <action-button
-            type="primary"
-            v-else
-            text="whatever I want"
-            @click="loginUser"
-          />
+          <action-button v-else text="Sign in" @click="loginUser" />
         </div>
       </div>
+
+      <the-subnav v-if="isLoggedIn" />
     </div>
   </header>
 </template>
@@ -39,12 +36,14 @@
 <script>
 import ActionButton from "@/components/ActionButton.vue";
 import ProfileImage from "@/components/ProfileImage.vue";
+import TheSubnav from "@/components/TheSubnav.vue";
 
 export default {
   name: "MainNav",
   components: {
     ActionButton,
     ProfileImage,
+    TheSubnav,
   },
   data() {
     return {
