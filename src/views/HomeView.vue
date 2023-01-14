743e9a94ec9e1565604bc20f86778aa363a9b1bf
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2023-01-15 02:51:06 +0900

    Add vue-router and setting

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <the-hero style="border: 1px solid red" />
+</template>
+
+<script>
+import TheHero from "@/components/JobSearch/TheHero.vue";
+
+export default {
+  name: "HomeView",
+  components: { TheHero },
+};
+</script>
