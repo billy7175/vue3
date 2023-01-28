25692b76ba918594b6635d45eeca8c361ed0b6eb
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2023-01-28 18:05:57 +0900

    Add getters && actions

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,12 +1,20 @@
 import { defineStore } from "pinia";
 
-export const useUserStore = defineStore('user', {
+export const ADD_SELECTED_ORGANIZATIONS = "ADD_SELECTED_ORGANIZATIONS";
+
+export const useUserStore = defineStore("user", {
   state: () => ({
-    isLoggedIn:false
+    isLoggedIn: false,
+    selectedOrganizations: [],
   }),
   actions: {
-    loginUser(){
+    loginUser() {
       this.isLoggedIn = true;
-    }
-  }
-})
+    },
+    [ADD_SELECTED_ORGANIZATIONS](organizations) {
+      console.log(123123);
+      console.log(organizations);
+      this.selectedOrganizations = organizations;
+    },
+  },
+});
